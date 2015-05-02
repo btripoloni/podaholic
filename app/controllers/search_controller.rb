b1d0b21aaf215e8709f541aaf9d96a50246ebd1f
Nando Sousa <nandosousafr@gmail.com>
2015-05-02 12:55:25 -0300

    adds podcast facet to search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @episodes = Episode.search params[:q].presence || '*', page: params[:page], per_page: 10, includes: :podcast
+    @episodes = Episode.search params[:q].presence || '*', page: params[:page], per_page: 10, includes: :podcast, facets: [:podcast_name]
   end
 end
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -5,4 +5,11 @@ class Episode < ActiveRecord::Base
   validates_presence_of :title, :media_url, :podcast_id
 
   delegate :name, to: :podcast, prefix: true
+
+  def search_data
+    attributes.merge(
+      podcast_name: podcast_name
+    )
+
+  end
 end
