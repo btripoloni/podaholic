27439c64679a29e67ca89d0cf963532ff5c35091
Nando Sousa <nandosousafr@gmail.com>
2015-05-03 13:43:11 -0300

    prevent errors when show notes is nil

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,8 +1,8 @@
 module SearchHelper
 
   def show_notes(details, episode_show_notes)
-    return episode_show_notes if details[:highlight].blank?
-    details[:highlight].fetch(:show_notes, episode_show_notes)
+    description = Hash(details[:highlight]).fetch(:show_notes, episode_show_notes).to_s
+    description.truncate(300).html_safe
   end
 end
 
