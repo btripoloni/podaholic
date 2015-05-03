44f009a010809526bf2eff25bb846fd9dd2977e6
Nando Sousa <nandosousafr@gmail.com>
2015-05-03 12:36:42 -0300

    prevent errors when encloses comes a nil value

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -21,28 +21,28 @@ class Feed
   def episodes
     response['item'].inject([]) do |arr, item|
       arr << OpenStruct.new(
-              title: item['title'],
-              link: item['link'],
-              description: item['description'],
-              summary: item['summary'],
-              raw_description: item['encoded'],
-              pub_date: Date.parse(item['pubDate']),
-              duration: item['duration'],
-              audio_url: resolve_audio_url(item['enclosure']),
-              image_url: item['image'].try(:url)
-            )
+        title: item['title'],
+        link: item['link'],
+        description: item['description'],
+        summary: item['summary'],
+        raw_description: item['encoded'],
+        pub_date: Date.parse(item['pubDate']),
+        duration: item['duration'],
+        audio_url: resolve_audio_url(item['enclosure']),
+        image_url: item['image'].try(:url)
+      )
     end
   end
 
   private
 
   def resolve_audio_url(enclosure)
-    audio = enclosure.is_a?(Array) ? enclosure.first : enclosure
+    audio = enclosure.is_a?(Array) ? enclosure.first : Hash(enclosure)
     audio['url']
   end
 
   def resolve_image_url(image_object)
-    img = image_object.is_a?(Array) ? image_object.last : image_object
+    img = image_object.is_a?(Array) ? image_object.last : Hash(image_object)
     img['href']
   end
 end
